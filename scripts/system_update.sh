f42295ff040d90e8e21d8eec7232b4a689edcfb6
paranoidporygon <dubious-disc.blog@proton.me>
2025-11-07 10:12:03 -0500

    updated update script

--- scripts/system_update.sh
+++ scripts/system_update.sh
@@ -4,18 +4,21 @@
 #sudo pacman-key --init
 #sudo pacman-key --populate
 
+###Update official packages###
 sudo pacman -S archlinux-keyring  # updates PGP signatures for repos
 sudo pacman -Syu # systemwide update
 
+###Update Yay### note: currently redundant since yay updates itself from the AUR
+#cd /tmp
+##sudo pacman -R yay-bin yay-bin-debug  # remove old yay bins
+#sudo pacman -S --needed git base-devel # make sure needed packages for compilation are installed
+#git clone https://aur.archlinux.org/yay.git  # clone yay from aur repo
+##cd yay-git
+##git clone https://aur.archlinux.org/yay  # clone yay from AUR
+#cd yay
+#makepkg -si  # reinstall yay from source
 
-cd /tmp
-#sudo pacman -R yay-bin yay-bin-debug  # remove old yay bins
-sudo pacman -S --needed git base-devel # make sure needed packages for compilation are installed
-git clone https://aur.archlinux.org/yay.git  # clone yay from aur repo
-#cd yay-git
-#git clone https://aur.archlinux.org/yay  # clone yay from AUR
-cd yay
-makepkg -si  # reinstall yay from source
+###Update AUR packages###
 yay -Syu  # update AUR packages now that official packages are already updated
 
 
