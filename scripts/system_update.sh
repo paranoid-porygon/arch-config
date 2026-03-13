9e3be9a3116754351cb522249dc836c8a38ad81d
paranoid-porygon <dubious-disc.blog@proton.me>
2026-03-13 12:52:58 -0400

    revised system update script

--- scripts/system_update.sh
+++ scripts/system_update.sh
@@ -4,11 +4,13 @@
 #sudo pacman-key --init
 #sudo pacman-key --populate
 
-###Update official packages###
-sudo pacman -S archlinux-keyring  # updates PGP signatures for repos
-sudo pacman -Syu # systemwide update
+## Update official packages
+sudo pacman -Sy --needed archlinux-keyring  # updates PGP signatures for repos
+sudo pacman -Su # systemwide update
 
-###Update Yay### note: currently redundant since yay updates itself from the AUR
+## Update Yay
+### note: currently redundant since yay updates itself from the AUR
+# TODO check if yay doesn't exist and then install, otherwise update itself
 #cd /tmp
 ##sudo pacman -R yay-bin yay-bin-debug  # remove old yay bins
 #sudo pacman -S --needed git base-devel # make sure needed packages for compilation are installed
@@ -18,12 +20,15 @@ sudo pacman -Syu # systemwide update
 #cd yay
 #makepkg -si  # reinstall yay from source
 
-###Update AUR packages###
+## Update AUR packages
 yay -Syu  # update AUR packages now that official packages are already updated
 
 
 # clear package tempfiles
 #sudo pacman -Qdtq | sudo pacman -Rs -  # use with caution since this might be needed
 yay -Sc
+
+read -p "Press any key to reboot... " -n 1 -s -r
+echo -e "\n"
 
 shutdown -r now
